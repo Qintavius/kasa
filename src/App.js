241d7bbeaf551c18fd935b02c8febb12b5f85847
Qintavius <qtzaba@gmail.com>
2022-11-07 18:27:44 +0100

    [UPDATE] Import composants Header et Footer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import About from './pages/About/About';
 import Home from './pages/Home/Home';
 import Housing from './pages/Housing/Housing';
 import NotFound from './pages/Errors/NotFound/NotFound';
+import HeaderNavigation from './components/Header/HeaderNavigation';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <HeaderNavigation />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/housing' element={<Housing />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
