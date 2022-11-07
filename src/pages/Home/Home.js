66a017e1afe5967497df3cbfbd089b5c3c3d85fc
Qintavius <qtzaba@gmail.com>
2022-11-07 18:21:46 +0100

    [ADD] Ajout style + composant Banner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../../components/Banner/Banner';
+import './Home.scss';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className='home'>
+            <Banner />
+        </section>
     );
 };
 
