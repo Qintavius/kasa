f6617b0fea91a84815ac5f3e711cdff6b1afda9b
Qintavius <qtzaba@gmail.com>
2022-11-08 19:47:15 +0100

    [ADD] Création composant carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Carousel.scss';
+
+const Carousel = (props) => {
+
+    const [currentImg, setCurrentImg] = useState(0);
+
+
+    return (
+        <div>
+            <div className='carousel-wrapper'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
