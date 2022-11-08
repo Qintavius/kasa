b339242058eade3f2bec60a6411b48865227a442
Qintavius <qtzaba@gmail.com>
2022-11-08 16:49:42 +0100

    [ADD] Création page fiche location

--- src/pages/Housing/Housing.js
+++ src/pages/Housing/Housing.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import FetchApi from '../../datas/FetchApi'
+import './Housing.scss';
+
 
 const Housing = () => {
+
+    const [housing, setHousing] = useState({});
+
+    const { id } = useParams();
+
+    useEffect(() => {
+
+        (async function fetchData() {
+            const data = await FetchApi.get('../logements.json')
+            const display = data.filter((location) => location.id === id)[0]
+
+            setHousing(display)
+        })()
+
+    }, [id])
+
     return (
-        <div>
-            <h1>Housing</h1>
+        <div className='housing-container'>
+            <img src={housing.pictures} alt={housing.title} />
+            <h2 className='housing-title'>{housing.title}</h2>
+            <h3 className='housing-location'>{housing.location}</h3>
+
         </div>
     );
 };
