5b8d31e7108663b0ff3b71ae3648bac131f5545c
Qintavius <qtzaba@gmail.com>
2022-11-08 16:53:22 +0100

    [ADD] Création page Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import FetchApi from '../../datas/FetchApi';
 import Banner from '../../components/Banner/Banner';
+import Card from '../../components/Card/Card';
 import './Home.scss';
 
 const Home = () => {
+
+    const [housing, setHousing] = useState([])
+
+    useEffect(() => {
+
+        (async function fetchData() {
+            const data = await FetchApi.get('../logements.json')
+            setHousing(data)
+        })()
+
+    }, [])
+
     return (
-        <section className='home'>
+        <section className='home-container'>
             <Banner />
+
+            <div className="cards">
+                {
+                    housing.map((house) =>
+                        <Card key={house.id} house={house} />)
+                }
+            </div>
         </section>
     );
 };
