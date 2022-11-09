50e22ced02940880c3a2beaef4d506a039714cf1
Qintavius <qtzaba@gmail.com>
2022-11-09 18:51:24 +0100

    [ADD] Création composant Rating stars

---
+++ src/components/Rating/Rating.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rating = (props) => {
+    return (
+        <div className='star-rating'>
+            <img src="./star.svg" alt="rating stars" />
+            <img src="./star.svg" alt="rating stars" />
+            <img src="./star.svg" alt="rating stars" />
+            <img src="./star.svg" alt="rating stars" />
+            <img src="./star.svg" alt="rating stars" />
+        </div>
+    );
+};
+
+export default Rating;
--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -8,6 +8,7 @@ import Tags from '../../components/Tags/Tags';
 import DropdownEquipments from '../../components/Dropdown/DropdownEquipments';
 import DropdownDescription from '../../components/Dropdown/DropdownDescription';
 import Host from '../../components/Host/Host';
+import Rating from '../../components/Rating/Rating';
 
 
 const Housing = () => {
@@ -33,6 +34,7 @@ const Housing = () => {
             <h2 className='housing-title'>{housing.title}</h2>
             <h3 className='housing-location'>{housing.location}</h3>
             <Tags tags={housing.tags} />
+            <Rating />
             <Host name={housing.host && housing.host.name} img={housing.host && housing.host.picture} />
             <div className="dropdown-container">
                 <DropdownDescription description={housing.description} />
