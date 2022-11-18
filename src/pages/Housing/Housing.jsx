c27d59565298493152d3c091d3fc8b9d362757d8
Qintavius <qtzaba@gmail.com>
2022-11-18 17:59:48 +0100

    [UPDATE] Modification line 20 = .filter remplacé par .find + suppr useEffect + add Navigate pour redirection page 404

--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -1,10 +1,8 @@
 import './Housing.scss';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import Carousel from '../../components/Carousel/Carousel';
 import Tags from '../../components/Tags/Tags';
-import DropdownEquipments from '../../components/Dropdown/DropdownEquipments';
-import DropdownDescription from '../../components/Dropdown/DropdownDescription';
+import Dropdown from '../../components/Dropdown/Dropdown';
 import Host from '../../components/Host/Host';
 import Rating from '../../components/Rating/Rating';
 import logements from './../../datas/logements.json';
@@ -12,35 +10,38 @@ import logements from './../../datas/logements.json';
 
 const Housing = () => {
 
-    const [housing, setHousing] = useState(logements);
-
     const { id } = useParams();
 
-    useEffect(() => {
-            const display = logements.filter((house) => house.id === id)[0]
-            setHousing(display)
-    }, [id])
+    const display = logements.find((house) => house.id === id)
+
+    if(!display) {
+        return <Navigate to="/404" />
+    }
 
     return (
         <div className='housing-container'>
-            <Carousel img={housing.pictures} />
+            <Carousel img={display.pictures} />
 
             <div className="housing-inner-container">
                 <div className="housing-title-location">
-                    <h2 className='housing-title'>{housing.title}</h2>
-                    <h3 className='housing-location'>{housing.location}</h3>
-                    <Tags tags={housing.tags} />
+                    <h2 className='housing-title'>{display.title}</h2>
+                    <h3 className='housing-location'>{display.location}</h3>
+                    <Tags tags={display.tags} />
                 </div>
 
                 <div className="rating-host-container">
-                    <Rating rating={housing.rating}/>
-                    <Host name={housing.host && housing.host.name} img={housing.host && housing.host.picture} />
+                    <Rating rating={display.rating}/>
+                    <Host name={display.host && display.host.name} img={display.host && display.host.picture} />
                 </div>
             </div>
             
             <div className="dropdown-container">
-                <DropdownDescription title="description" description={housing.description} />
-                <DropdownEquipments title="équipements" equipments={housing.equipments} />
+                <Dropdown title="description">{display.description}</Dropdown>
+                <Dropdown title="équipements">
+                    {display.equipments.map((house) => (
+                        <div key={house}>{house}</div>
+                    ))}
+                </Dropdown>
             </div>
         </div>
     );
